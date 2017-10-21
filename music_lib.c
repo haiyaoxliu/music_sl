91c8df74d907fb0b495a4fe9e1f94ed9bed4c652
hliu3 <hliu3@stuy.edu>
2017-10-20 21:59:34 -0400

    rename

--- music_lib.c
+++ music_lib.c
@@ -6,6 +6,8 @@
 
 struct song_node *table[26];
 
+
+
 void insert_song(song_node *p) {
   if (!p) {
     table[(p -> artist)[0]-'a'] = insert_front(table[(p -> artist)[0]-'a'], p);
@@ -42,8 +44,56 @@ struct song_node * find_artist(song_node *p) {
   return find_artist_list(table[(p -> artist)[0]-'a'], p);
 }
 
+struct song_node * find_artist_list(song_node h, song_node *p) {
+  if (!h) {
+    return h;
+  }
+  while (strcmp(p -> artist, h -> artist) > 0) {
+    h = h -> next;
+  }
+  if (!strcmp(p -> artist, h -> artist)) {
+    return h;
+  }
+  else {
+    return NULL;
+  }
+}
 
+void print_letter(char c) {
+  print_list(table[c-'a']);
+}
 
+void print_library() {
+  int i;
+  for (i = 0; i < 26, i++) {
+    if (table[i]) {
+      print_letter('a' + i);
+    }
+  }
+}
+
+struct song_node * delete_song(song_node *p) {
+  return delete_song_list(table[(p -> artist)[0]-'a'], p);
+}
+
+struct song_node * delete_song_list(song_node h, song_node *p) {
+  if (!h) {
+    return h;
+  }
+  while (strcmp(p -> artist, h -> artist) > 0) {
+    h = h -> next;
+  }
+  while (!(strcmp(p -> artist, h -> artist)) && strcmp(p -> name, h -> name) > 0) {
+    h = h -> next;
+  }
+  if (!strcmp(p -> artist, h -> artist) && !strcmp(p -> name, h -> name)) {
+    
+  }
+  else {
+    return NULL;
+  }
+}
+  
 
 
 
