774e78d3c32974027fb982fdd196cdb9e69049c7
hliu3 <hliu3@stuy.edu>
2017-10-20 21:31:44 -0400

    cleanup

---
+++ list.h
@@ -0,0 +1,11 @@
+struct song_node {
+  char name[256];
+  char artist[256];
+  struct song_node *next;
+};
+
+void print_list(struct song_node *);
+int song_comp(struct song_node *, struct song_node *);
+struct song_node * insert_order(struct song_node *, char *, char *);
+struct song_node * insert_front(struct song_node *, char *, char *);
+struct song_node * free_list(struct song_node *);
