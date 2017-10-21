645f9406bf6c5d3a885e441fdc3c3de8b0cbecfd
hliu3 <hliu3@stuy.edu>
2017-10-20 21:44:26 -0400

    oops

--- list.h
+++ list.h
@@ -9,3 +9,4 @@ int song_comp(struct song_node *, struct song_node *);
 struct song_node * insert_order(struct song_node *, struct song_node *);
 struct song_node * insert_front(struct song_node *, struct song_node *);
 struct song_node * free_list(struct song_node *);
+struct song_node * create(char [], char []);
